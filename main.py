eb718e6bb1c118371066bb9262811e7cbf2f5b0a
kleber89 <benkosisackson@gmail.com>
2024-07-23 01:00:57 -0500

    new changes in code

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-""
+from flask.cli import FlaskGroup
+from src import create_app
+
+# Create Flask application instance
+app = create_app()
+
+# Create CLI group instance
+cli = FlaskGroup(create_app=create_app)
+
+if __name__ == "__main__":
+    cli()
